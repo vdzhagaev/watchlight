f6911b4d05fd06386267806b5175730b686d0a38
vdzhagaev <slava.dzhagaev@proton.me>
2026-06-03 15:01:31 +0300

    feat(service/checker): improve CheckRequest and add CheckResult, Checker interface with check func

--- internal/services/checker/checker.go
+++ internal/services/checker/checker.go
@@ -1,8 +1,24 @@
 package checker
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type CheckRequest struct {
-	URL     string        `json:"url"`
-	Timeout time.Duration `json:"timeout"`
+	URL      string
+	Timeout  time.Duration
+	Keywords []string
+}
+
+type CheckResult struct {
+	ResponseTime  time.Duration
+	StatusCode    int
+	FoundKeywords []string
+	Reachable     bool
+	Err           error
+}
+
+type Checker interface {
+	Check(ctx context.Context, req CheckRequest) (CheckResult, error)
 }
